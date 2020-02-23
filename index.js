754cf08a22f8a75cf125b1c0c9f9794aa2cbdc03
debian <debian@debian>
2020-02-23 19:14:34 +0100

    lint: updated eslintrc files, added newline at the end of files, fixed "npm run lint" script, linted all JavaScript and Markdown files

--- index.js
+++ index.js
@@ -1,125 +1,124 @@
 'use strict'
 
-let _errors = {
-    'BadRequest': {
-      'statusCode': 400,
-      'type': 'BadRequest',
-      'message': 'Bad request'
-    },
-    'ValidationError': {
-      'statusCode': 400,
-      'type': 'BadRequest',
-      'message': 'ValidationError: your request contains invalid data.'
-    },
-    'Unauthorized': {
-      'statusCode': 401,
-      'type': 'Unauthorized',
-      'message': 'Not authorized'
-    },
-    'PaymentRequired': {
-      'statusCode': 402,
-      'type': 'PaymentRequired',
-      'message': 'Payment required'
-    },
-    'Forbidden': {
-      'statusCode': 403,
-      'type': 'Forbidden',
-      'message': "Client has valid credentials, but doesn't have access to this action."
-    },
-    'NotFound': {
-      'statusCode': 404,
-      'type': 'NotFound',
-      'message': 'Not found'
-    },
-    'MethodNotAllowed': {
-      'statusCode': 405,
-      'type': 'MethodNotAllowed',
-      'message': 'Method not allowed'
-    },
-    'NotAcceptable': {
-      'statusCode': 406,
-      'type': 'NotAcceptable',
-      'message': "The request's format is not acceptable. Check Content-Type header for a list of acceptable formats."
-    },
-    'RequestTimeout': {
-      'statusCode': 408,
-      'type': 'RequestTimeout',
-      'message': 'The server timed out waiting for the request.'
-    },
-    'Conflict': {
-      'statusCode': 409,
-      'type': 'Conflict',
-      'message': 'The request could not be completed due to a conflict with the current state of the resource.'
-    },
-    'LengthRequired': {
-      'statusCode': 411,
-      'type': 'LengthRequired',
-      'message': 'The request did not specify the length of its content, which is required by the requested resource.'
-    },
-    'PreconditionFailed': {
-      'statusCode': 412,
-      'type': 'PreconditionFailed',
-      'message': 'The server does not meet one of the preconditions that the requester put on the request.'
-    },
-    'EntityTooLarge': {
-      'statusCode': 413,
-      'type': 'EntityTooLarge',
-      'message': 'The request entity is too large to be processed.'
-    },
-    'PayloadTooLarge': {
-      'statusCode': 413,
-      'type': 'PayloadTooLarge',
-      'message': 'The request entity is too large to be processed.'
-    },
-    'URITooLong': {
-      'statusCode': 414,
-      'type': 'URITooLong',
-      'message': 'The URI provided was too long for the server to process.'
-    },
-    'UnsupportedMediaType': {
-      'statusCode': 415,
-      'type': 'UnsupportedMediaType',
-      'message': 'The request entity has a media type which the server or resource does not support.'
-    },
-    'UnprocessableEntity': {
-      'statusCode': 422,
-      'type': 'UnprocessableEntity',
-      'message': 'The syntax of the request is correct but some values could be invalid (e.g. wrong type).'
-    },
-    'TooManyRequests': {
-      'statusCode': 429,
-      'type': 'TooManyRequests',
-      'message': 'The user has sent too many requests in a given amount of time. more info at GET /v0/rate'
-    },
-    'RequestHeaderFieldsTooLarge': {
-      'statusCode': 432,
-      'type': 'RequestHeaderFieldsTooLarge',
-      'message': 'The server is unwilling to process the request because its header fields are too large.'
-    },
-    'InternalServerError': {
-      'statusCode': 500,
-      'type': 'InternalServerError',
-      'message': 'Internal server error'
-    },
-    'NotImplemented': {
-      'statusCode': 501,
-      'type': 'NotImplemented',
-      'message': 'This endpoint has not been implemented'
-    },
-    'BadGateway': {
-      'statusCode': 502,
-      'type': 'BadGateway',
-      'message': 'This service received an invalid response from an upstream service.'
-    }
+const _errors = {
+  BadRequest: {
+    statusCode: 400,
+    type: 'BadRequest',
+    message: 'Bad request'
+  },
+  ValidationError: {
+    statusCode: 400,
+    type: 'BadRequest',
+    message: 'ValidationError: your request contains invalid data.'
+  },
+  Unauthorized: {
+    statusCode: 401,
+    type: 'Unauthorized',
+    message: 'Not authorized'
+  },
+  PaymentRequired: {
+    statusCode: 402,
+    type: 'PaymentRequired',
+    message: 'Payment required'
+  },
+  Forbidden: {
+    statusCode: 403,
+    type: 'Forbidden',
+    message: "Client has valid credentials, but doesn't have access to this action."
+  },
+  NotFound: {
+    statusCode: 404,
+    type: 'NotFound',
+    message: 'Not found'
+  },
+  MethodNotAllowed: {
+    statusCode: 405,
+    type: 'MethodNotAllowed',
+    message: 'Method not allowed'
+  },
+  NotAcceptable: {
+    statusCode: 406,
+    type: 'NotAcceptable',
+    message: "The request's format is not acceptable. Check Content-Type header for a list of acceptable formats."
+  },
+  RequestTimeout: {
+    statusCode: 408,
+    type: 'RequestTimeout',
+    message: 'The server timed out waiting for the request.'
+  },
+  Conflict: {
+    statusCode: 409,
+    type: 'Conflict',
+    message: 'The request could not be completed due to a conflict with the current state of the resource.'
+  },
+  LengthRequired: {
+    statusCode: 411,
+    type: 'LengthRequired',
+    message: 'The request did not specify the length of its content, which is required by the requested resource.'
+  },
+  PreconditionFailed: {
+    statusCode: 412,
+    type: 'PreconditionFailed',
+    message: 'The server does not meet one of the preconditions that the requester put on the request.'
+  },
+  EntityTooLarge: {
+    statusCode: 413,
+    type: 'EntityTooLarge',
+    message: 'The request entity is too large to be processed.'
+  },
+  PayloadTooLarge: {
+    statusCode: 413,
+    type: 'PayloadTooLarge',
+    message: 'The request entity is too large to be processed.'
+  },
+  URITooLong: {
+    statusCode: 414,
+    type: 'URITooLong',
+    message: 'The URI provided was too long for the server to process.'
+  },
+  UnsupportedMediaType: {
+    statusCode: 415,
+    type: 'UnsupportedMediaType',
+    message: 'The request entity has a media type which the server or resource does not support.'
+  },
+  UnprocessableEntity: {
+    statusCode: 422,
+    type: 'UnprocessableEntity',
+    message: 'The syntax of the request is correct but some values could be invalid (e.g. wrong type).'
+  },
+  TooManyRequests: {
+    statusCode: 429,
+    type: 'TooManyRequests',
+    message: 'The user has sent too many requests in a given amount of time. more info at GET /v0/rate'
+  },
+  RequestHeaderFieldsTooLarge: {
+    statusCode: 432,
+    type: 'RequestHeaderFieldsTooLarge',
+    message: 'The server is unwilling to process the request because its header fields are too large.'
+  },
+  InternalServerError: {
+    statusCode: 500,
+    type: 'InternalServerError',
+    message: 'Internal server error'
+  },
+  NotImplemented: {
+    statusCode: 501,
+    type: 'NotImplemented',
+    message: 'This endpoint has not been implemented'
+  },
+  BadGateway: {
+    statusCode: 502,
+    type: 'BadGateway',
+    message: 'This service received an invalid response from an upstream service.'
   }
+}
 
 /**
  * @class Base class for HTTP Errors.
  */
 class HTTPError extends Error {
   constructor (data) {
-
-    data = data || Object.assign({},_errors["InternalServerError"])
+    data = data || Object.assign({}, _errors.InternalServerError)
     // Calling parent constructor of base Error class.
     super(data.message)
 
@@ -142,13 +141,13 @@ class HTTPError extends Error {
     Error.captureStackTrace(this, this.constructor)
   }
 
-  toJSON() {
-    let self = this;
+  toJSON () {
+    const self = this
 
     return {
-      statusCode : self.statusCode,
-      type : self.type,
-      message : self._message,
+      statusCode: self.statusCode,
+      type: self.type,
+      message: self._message,
       name: self.name
     }
   }
@@ -156,160 +155,160 @@ class HTTPError extends Error {
 
 class BadRequest extends HTTPError {
   constructor (message) {
-    let params = Object.assign({},_errors.BadRequest)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.BadRequest)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class Unauthorized extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.Unauthorized)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.Unauthorized)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class PaymentRequired extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.PaymentRequired)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.PaymentRequired)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class Forbidden extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.Forbidden)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.Forbidden)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class NotFound extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.NotFound)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.NotFound)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class MethodNotAllowed extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.MethodNotAllowed)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.MethodNotAllowed)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class NotAcceptable extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.NotAcceptable)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.NotAcceptable)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class RequestTimeout extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.RequestTimeout)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.RequestTimeout)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class Conflict extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.Conflict)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.Conflict)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class LengthRequired extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.LengthRequired)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.LengthRequired)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class PreconditionFailed extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.PreconditionFailed)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.PreconditionFailed)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class EntityTooLarge extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.EntityTooLarge)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.EntityTooLarge)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class URITooLong extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.URITooLong)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.URITooLong)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class UnsupportedMediaType extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.UnsupportedMediaType)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.UnsupportedMediaType)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class UnprocessableEntity extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.UnprocessableEntity)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.UnprocessableEntity)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class TooManyRequests extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.TooManyRequests)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.TooManyRequests)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class RequestHeaderFieldsTooLarge extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.RequestHeaderFieldsTooLarge)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.RequestHeaderFieldsTooLarge)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class InternalServerError extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.InternalServerError)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.InternalServerError)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class NotImplemented extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.NotImplemented)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.NotImplemented)
+    params.message = message || params.message
     super(params)
   }
 }
 
 class BadGateway extends HTTPError {
   constructor (message) {
-    let params = Object.assign({}, _errors.BadGateway)
-    params.message = message || params.message;
+    const params = Object.assign({}, _errors.BadGateway)
+    params.message = message || params.message
     super(params)
   }
 }
--- test/errors.spec.js
+++ test/errors.spec.js
@@ -3,32 +3,31 @@
 const Errors = require('../index')
 const chai = require('chai')
 
-let expect = chai.expect
+const expect = chai.expect
 
 describe('HTTP Errors Unit Tests', function () {
   it('Error should be instance of HTTPError', function () {
-    let err = new Errors.BadRequest()
+    const err = new Errors.BadRequest()
     expect(err instanceof Errors.HTTPError).to.equal(true)
   })
 
   it('Should correctly set an error message', function () {
-    let err = new Errors.BadRequest('Terrible Mistake')
-
+    const err = new Errors.BadRequest('Terrible Mistake')
     expect(err.message).to.equal('Terrible Mistake')
   })
 
   it('Should correctly set an error status code', function () {
-    let err = new Errors.BadRequest()
+    const err = new Errors.BadRequest()
     expect(err.statusCode).to.equal(400)
   })
 
   it('Should correctly default to 500 error', function () {
-    let err = new Errors.HTTPError()
+    const err = new Errors.HTTPError()
     expect(err.statusCode).to.equal(500)
   })
 
   it('Should correctly return the correct JSON', function () {
-    let err = new Errors.BadRequest()
+    const err = new Errors.BadRequest()
     expect(err.toJSON()).to.deep.equal({
       statusCode: 400,
       type: 'BadRequest',
@@ -38,121 +37,121 @@ describe('HTTP Errors Unit Tests', function () {
   })
 
   it('400 BadRequest', function () {
-    let err = new Errors.BadRequest()
+    const err = new Errors.BadRequest()
     expect(err.statusCode).to.equal(400)
     expect(err.type).to.equal('BadRequest')
   })
 
   it('401 Unauthorized', function () {
-    let err = new Errors.Unauthorized()
+    const err = new Errors.Unauthorized()
     expect(err.statusCode).to.equal(401)
     expect(err.type).to.equal('Unauthorized')
   })
 
   it('402 PaymentRequired', function () {
-    let err = new Errors.PaymentRequired()
+    const err = new Errors.PaymentRequired()
     expect(err.statusCode).to.equal(402)
     expect(err.type).to.equal('PaymentRequired')
   })
 
   it('403 Forbidden', function () {
-    let err = new Errors.Forbidden()
+    const err = new Errors.Forbidden()
     expect(err.statusCode).to.equal(403)
     expect(err.type).to.equal('Forbidden')
   })
 
   it('404 NotFound', function () {
-    let err = new Errors.NotFound()
+    const err = new Errors.NotFound()
     expect(err.statusCode).to.equal(404)
     expect(err.type).to.equal('NotFound')
   })
 
   it('405 MethodNotAllowed', function () {
-    let err = new Errors.MethodNotAllowed()
+    const err = new Errors.MethodNotAllowed()
     expect(err.statusCode).to.equal(405)
     expect(err.type).to.equal('MethodNotAllowed')
   })
 
   it('406 NotAcceptable', function () {
-    let err = new Errors.NotAcceptable()
+    const err = new Errors.NotAcceptable()
     expect(err.statusCode).to.equal(406)
     expect(err.type).to.equal('NotAcceptable')
   })
 
   it('408 RequestTimeout', function () {
-    let err = new Errors.RequestTimeout()
+    const err = new Errors.RequestTimeout()
     expect(err.statusCode).to.equal(408)
     expect(err.type).to.equal('RequestTimeout')
   })
 
   it('409 Conflict', function () {
-    let err = new Errors.Conflict()
+    const err = new Errors.Conflict()
     expect(err.statusCode).to.equal(409)
     expect(err.type).to.equal('Conflict')
   })
 
   it('411 LengthRequired', function () {
-    let err = new Errors.LengthRequired()
+    const err = new Errors.LengthRequired()
     expect(err.statusCode).to.equal(411)
     expect(err.type).to.equal('LengthRequired')
   })
 
   it('412 PreconditionFailed', function () {
-    let err = new Errors.PreconditionFailed()
+    const err = new Errors.PreconditionFailed()
     expect(err.statusCode).to.equal(412)
     expect(err.type).to.equal('PreconditionFailed')
   })
 
   it('413 EntityTooLarge', function () {
-    let err = new Errors.EntityTooLarge()
+    const err = new Errors.EntityTooLarge()
     expect(err.statusCode).to.equal(413)
     expect(err.type).to.equal('EntityTooLarge')
   })
 
   it('414 URITooLong', function () {
-    let err = new Errors.URITooLong()
+    const err = new Errors.URITooLong()
     expect(err.statusCode).to.equal(414)
     expect(err.type).to.equal('URITooLong')
   })
 
   it('415 UnsupportedMediaType', function () {
-    let err = new Errors.UnsupportedMediaType()
+    const err = new Errors.UnsupportedMediaType()
     expect(err.statusCode).to.equal(415)
     expect(err.type).to.equal('UnsupportedMediaType')
   })
 
   it('422 UnprocessableEntity', function () {
-    let err = new Errors.UnprocessableEntity()
+    const err = new Errors.UnprocessableEntity()
     expect(err.statusCode).to.equal(422)
     expect(err.type).to.equal('UnprocessableEntity')
   })
 
   it('429 TooManyRequests', function () {
-    let err = new Errors.TooManyRequests()
+    const err = new Errors.TooManyRequests()
     expect(err.statusCode).to.equal(429)
     expect(err.type).to.equal('TooManyRequests')
   })
 
   it('432 RequestHeaderFieldsTooLarge', function () {
-    let err = new Errors.RequestHeaderFieldsTooLarge()
+    const err = new Errors.RequestHeaderFieldsTooLarge()
     expect(err.statusCode).to.equal(432)
     expect(err.type).to.equal('RequestHeaderFieldsTooLarge')
   })
 
   it('500 InternalServerError', function () {
-    let err = new Errors.InternalServerError()
+    const err = new Errors.InternalServerError()
     expect(err.statusCode).to.equal(500)
     expect(err.type).to.equal('InternalServerError')
   })
 
   it('501 NotImplemented', function () {
-    let err = new Errors.NotImplemented()
+    const err = new Errors.NotImplemented()
     expect(err.statusCode).to.equal(501)
     expect(err.type).to.equal('NotImplemented')
   })
 
   it('502 BadGateway', function () {
-    let err = new Errors.BadGateway()
+    const err = new Errors.BadGateway()
     expect(err.statusCode).to.equal(502)
     expect(err.type).to.equal('BadGateway')
   })
